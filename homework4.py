1d1edfdf6a15ce94d2547ad5c4a9ddfc030838fb
Nazar Novak <Nazar.Novak@harmonicinc.com>
2021-05-27 12:24:27 +0300

    исправлено задание 4

--- homework4.py
+++ homework4.py
@@ -7,3 +7,4 @@ height_float = float(input('Print your height (in meters): '))
 print('Your height is', height_float, 'meter(s) (float value)')
 print('Your height is', int(height_float), 'meter(s) (integer value)')
 print('Your height is', str(height_float), 'meter(s) (string value)')
+
